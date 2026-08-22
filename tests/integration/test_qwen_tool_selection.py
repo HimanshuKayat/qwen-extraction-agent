dc79cd13fd03c54cf0d740d9161f4543591ac06c
HimanshuKayat <himanshukayat7@gmail.com>
2026-08-22 19:34:08 +0530

    Update test_qwen_tool_selection.py

--- tests/integration/test_qwen_tool_selection.py
+++ tests/integration/test_qwen_tool_selection.py
@@ -3,7 +3,7 @@ import json
 from agent.model import QwenModel
 from agent.parser import parse_action
 from agent.prompts import build_tool_selection_messages
-from tools.registry import build_registry
+from tools.definitions import build_registry
 
 
 def test_qwen_selects_http_download():
